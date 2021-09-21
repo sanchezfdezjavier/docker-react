cc02e53c53bc31478efa980ba2a393a4afd5c353
Javier Sanchez <sanchezfdez.javier@gmail.com>
2021-09-21 20:51:46 +0100

    Lesson 82

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Changed another time v2!
         </p>
         <a
           className="App-link"
